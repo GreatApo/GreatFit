c33b8bdbfbf87e3a8fb61b0db5b0c373be55c672
Grammatopoulos Apostolos <greatapo@gmail.com>
2018-09-15 19:53:58 +0300

    Hugarian translation fix by gidano

--- app/src/main/java/com/dinodevs/greatfitwatchface/widget/MainClock.java
+++ app/src/main/java/com/dinodevs/greatfitwatchface/widget/MainClock.java
@@ -79,7 +79,7 @@ public class MainClock extends DigitalClockWidget {
             {"SONNTAG", "MONTAG", "DIENSTAG", "MITTWOCH", "DONNERSTAG", "FREITAG", "SAMSTAG"},
             {"ΚΥΡΙΑΚΉ", "ΔΕΥΤΈΡΑ", "ΤΡΊΤΗ", "ΤΕΤΆΡΤΗ", "ΠΈΜΠΤΗ", "ΠΑΡΑΣΚΕΥΉ", "ΣΆΒΒΑΤΟ"},
             {"ש'","ו'","ה'","ד'","ג'","ב'","א'"},
-            {"VASÁRNAP", "HÉTFŐ", "KEDD", "SZERDA", "CSÜTÖRTÖK", "PÉNTEK", "SZOMBATON"},
+            {"VASÁRNAP", "HÉTFŐ", "KEDD", "SZERDA", "CSÜTÖRTÖK", "PÉNTEK", "SZOMBAT"},
             {"DOMENICA", "LUNEDÌ", "MARTEDÌ", "MERCOLEDÌ", "GIOVEDÌ", "VENERDÌ", "SABATO"},
             {"日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"},
             {"일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"},
