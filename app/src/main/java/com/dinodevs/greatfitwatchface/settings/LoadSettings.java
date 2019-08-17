2085e8332aa165f63e0b280e46b89fc36a7471da
Grammatopoulos Apostolos <greatapo@gmail.com>
2019-08-17 05:48:02 +0100

    Slpt clock only: true by default on verge

--- app/src/main/java/com/dinodevs/greatfitwatchface/settings/LoadSettings.java
+++ app/src/main/java/com/dinodevs/greatfitwatchface/settings/LoadSettings.java
@@ -535,7 +535,7 @@ public class LoadSettings {
 
             this.analog_clock = sharedPreferences.getBoolean( "analog_clock", context.getResources().getBoolean(R.bool.analog_clock));
             this.digital_clock = sharedPreferences.getBoolean( "digital_clock", context.getResources().getBoolean(R.bool.digital_clock));
-            this.clock_only_slpt = sharedPreferences.getBoolean( "clock_only_slpt", context.getResources().getBoolean(R.bool.clock_only_slpt));
+            this.clock_only_slpt = sharedPreferences.getBoolean( "clock_only_slpt", isVerge() || context.getResources().getBoolean(R.bool.clock_only_slpt));
 
             // Populate color codes
             String[] colorCodes = context.getResources().getStringArray(R.array.color_codes);
